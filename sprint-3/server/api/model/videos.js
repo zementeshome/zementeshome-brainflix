80e08912886a364a0fd1448a55b95e0b0c3fe814
Zemen Teshome <zementeshome@gmail.com>
2020-10-01 19:47:42 -0400

    back end api model set up

---
+++ sprint-3/server/api/model/videos.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = './data.json';
+
+class Video {
+    readData = () => {
+        // once we get the data we're resolving the promise with the data
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, 'utf-8', (error, data) => {
+                resolve(JSON.parse(data))
+        })    // console.log( JSON.parse(data))
+      })
+    }
+    getVideoDetailsById = async (videoId) => {
+       const data =  await this.readData()
+    //    console.log(videoId, data)
+      return data.videos.find((video => 
+       video.id == videoId
+       ))
+    }
+
+    // going through array and trying to find the video with the id the function is recieving
+
+    getVideos = () => {
+        return this.readData()
+    }
+}
+
+module.exports = Video;
--- sprint-3/server/server.js
+++ sprint-3/server/server.js
@@ -1,16 +1,34 @@
 const express = require('express')
-const app = express()
-// const routes = require('./routes')
+const app = express();
+const Video = require('./api/model/videos');
+const VideoData = new Video();
+// const routes = require('./routes';
 
-// const port = process.env.PORT || process.argv[2] || 8022
+// app.use((req, resp, next) => {
+//     resp.setHeader('Access-Control-Allow-Origin', '*');
+    
+//     next();
+//     }); // middleware to set the response header
 
-// app.listen(port, () => console.log(`Listening on ${port}`))
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+})
 
-// app.get('/', function(req, res) {
-//     res.send('Hello World!')
-// })
-// app.use('/', routes)
-// app.use('/videos/:id', routes)
-// app.use('/videoupload', routes)
+app.get ('/videos', async function(req, res) {
+    // VideoData.getVideoDetailsById()
+    const videos = await VideoData.getVideos()
+    res.status(200).json(videos)
+
+})
+
+app.get ('/videos/:id', async function(req, res) {
+    const videos = await VideoData.getVideoDetailsById(req.params['id'])
+    console.log(videos, 'hey')
+    res.status(200).json(videos)
+    // console.log(req.params)
+    // const videos = await VideoData.getVideos()
+    // res.status(200).json(videos)
+
+})
 
 app.listen(8022, () => console.log("running on port 8022"));
